2ad44de5551abb62485561884e0b8ee2c5449c53
amintt2 <amin2020e@gmail.com>
2025-01-17 10:43:58 +0100

    No changes detected in the current file.

---
+++ sae_sql.sql
@@ -0,0 +1,4 @@
+-- ce script contient une première version de la base de données du projet et un jeu de test en fonction de votre projet
+-- Créer des enregistrements pour les articles (15 minimum)
+-- Rechercher des images (photos) pour ces différents articles (15 photos différentes minimum, ne pas avoir la même photo pour plusieurs articles)
+
