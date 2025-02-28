0e3fe2a3ef3c6bc20a863223578a6e17b5554e31
amintt2 <amin2020e@gmail.com>
2025-02-28 20:49:41 +0100

    Refactor admin article and comment management with improved error handling and UI

--- controllers/admin_article.py
+++ controllers/admin_article.py
@@ -19,26 +19,26 @@ def show_article():
     mycursor = get_db().cursor()
     sql = '''
     SELECT 
-        s.id_skin as id_article,
-        s.nom_skin as nom,
-        s.prix_skin as prix,
-        s.stock,
-        s.image,
-        s.description,
-        ts.id_type_skin as type_article_id,
-        ts.libelle_type_skin as libelle,
-        MIN(s2.stock) as min_stock,
-        COUNT(DISTINCT c.id_commande) as nb_commandes,
-        COUNT(DISTINCT lp.utilisateur_id) as nb_paniers
-    FROM skin s
-    LEFT JOIN type_skin ts ON s.type_skin_id = ts.id_type_skin
-    LEFT JOIN skin s2 ON s.type_skin_id = s2.type_skin_id
-    LEFT JOIN ligne_commande lc ON s.id_skin = lc.skin_id
-    LEFT JOIN commande c ON lc.commande_id = c.id_commande
-    LEFT JOIN ligne_panier lp ON s.id_skin = lp.skin_id
-    GROUP BY s.id_skin, s.nom_skin, s.prix_skin, s.stock, s.image, 
-             s.description, ts.id_type_skin, ts.libelle_type_skin
-    ORDER BY s.nom_skin
+        skin.id_skin as id_article,
+        skin.nom_skin as nom,
+        skin.prix_skin as prix,
+        skin.stock,
+        skin.image,
+        skin.description,
+        type_skin.id_type_skin as type_article_id,
+        type_skin.libelle_type_skin as libelle,
+        MIN(skin2.stock) as min_stock,
+        COUNT(DISTINCT commande.id_commande) as nb_commandes,
+        COUNT(DISTINCT ligne_panier.utilisateur_id) as nb_paniers
+    FROM skin
+    LEFT JOIN type_skin ON skin.type_skin_id = type_skin.id_type_skin
+    LEFT JOIN skin skin2 ON skin.type_skin_id = skin2.type_skin_id
+    LEFT JOIN ligne_commande ON skin.id_skin = ligne_commande.skin_id
+    LEFT JOIN commande ON ligne_commande.commande_id = commande.id_commande
+    LEFT JOIN ligne_panier ON skin.id_skin = ligne_panier.skin_id
+    GROUP BY skin.id_skin, skin.nom_skin, skin.prix_skin, skin.stock, skin.image, 
+             skin.description, type_skin.id_type_skin, type_skin.libelle_type_skin
+    ORDER BY skin.nom_skin
     '''
     mycursor.execute(sql)
     articles = mycursor.fetchall()
@@ -79,7 +79,6 @@ def add_article():
 @admin_article.route('/admin/article/add', methods=['POST'])
 def valid_add_article():
     mycursor = get_db().cursor()
-
     nom = request.form.get('nom', '')
     type_article_id = request.form.get('type_article_id', '')
     prix = request.form.get('prix', '')
@@ -114,102 +113,134 @@ def valid_add_article():
 
 @admin_article.route('/admin/article/delete', methods=['GET'])
 def delete_article():
-    id_article=request.args.get('id_article')
+    id_article = request.args.get('id_article')
     mycursor = get_db().cursor()
-    sql = ''' requête admin_article_3 '''
-    mycursor.execute(sql, id_article)
-    nb_declinaison = mycursor.fetchone()
-    if nb_declinaison['nb_declinaison'] > 0:
-        message= u'il y a des declinaisons dans cet article : vous ne pouvez pas le supprimer'
-        flash(message, 'alert-warning')
-    else:
-        sql = ''' requête admin_article_4 '''
-        mycursor.execute(sql, id_article)
-        article = mycursor.fetchone()
-        print(article)
-        image = article['image']
+    
+    # Vérifier si l'article est dans un panier
+    sql = '''
+    SELECT COUNT(*) as count 
+    FROM ligne_panier 
+    WHERE skin_id = %s
+    '''
+    mycursor.execute(sql, (id_article,))
+    count_panier = mycursor.fetchone()['count']
 
-        sql = ''' requête admin_article_5  '''
-        mycursor.execute(sql, id_article)
-        get_db().commit()
-        if image != None:
-            os.remove('static/images/' + image)
+    # Vérifier si l'article est dans une commande
+    sql = '''
+    SELECT COUNT(*) as count 
+    FROM ligne_commande 
+    WHERE skin_id = %s
+    '''
+    mycursor.execute(sql, (id_article,))
+    count_commande = mycursor.fetchone()['count']
 
-        print("un article supprimé, id :", id_article)
-        message = u'un article supprimé, id : ' + id_article
-        flash(message, 'alert-success')
+    if count_panier > 0:
+        flash("Impossible de supprimer l'article : il est présent dans un panier", 'error')
+        return redirect('/admin/article/show')
 
+    if count_commande > 0:
+        flash("Impossible de supprimer l'article : il est présent dans une commande", 'error')
+        return redirect('/admin/article/show')
+    
+    # Récupérer l'image avant la suppression
+    sql = '''
+    SELECT image 
+    FROM skin 
+    WHERE id_skin = %s
+    '''
+    mycursor.execute(sql, (id_article,))
+    image = mycursor.fetchone()['image']
+    
+    # Supprimer l'article
+    sql = '''
+    DELETE FROM skin 
+    WHERE id_skin = %s
+    '''
+    mycursor.execute(sql, (id_article,))
+    get_db().commit()
+    
+    # Supprimer l'image si elle existe
+    if image and os.path.exists(os.path.join('static/images/', image)):
+        os.remove(os.path.join('static/images/', image))
+    
+    flash('Article supprimé avec succès', 'success')
     return redirect('/admin/article/show')
 
 
 @admin_article.route('/admin/article/edit', methods=['GET'])
 def edit_article():
-    id_article=request.args.get('id_article')
+    id_article = request.args.get('id_article')
     mycursor = get_db().cursor()
     sql = '''
-    requête admin_article_6    
+    SELECT 
+        skin.id_skin as id_article,
+        skin.nom_skin as nom,
+        skin.prix_skin as prix,
+        skin.stock,
+        skin.image,
+        skin.description,
+        skin.type_skin_id as type_article_id,
+        type_skin.libelle_type_skin as libelle
+    FROM skin
+    LEFT JOIN type_skin ON skin.type_skin_id = type_skin.id_type_skin
+    WHERE skin.id_skin = %s
     '''
-    mycursor.execute(sql, id_article)
+    mycursor.execute(sql, (id_article,))
     article = mycursor.fetchone()
-    print(article)
     sql = '''
-    requête admin_article_7
+    SELECT id_type_skin as id_type_article, libelle_type_skin as libelle 
+    FROM type_skin
+    ORDER BY libelle_type_skin
     '''
     mycursor.execute(sql)
     types_article = mycursor.fetchall()
 
-    # sql = '''
-    # requête admin_article_6
-    # '''
-    # mycursor.execute(sql, id_article)
-    # declinaisons_article = mycursor.fetchall()
-
-    return render_template('admin/article/edit_article.html'
-                           ,article=article
-                           ,types_article=types_article
-                         #  ,declinaisons_article=declinaisons_article
-                           )
+    return render_template('admin/article/edit_article.html',
+                         article=article,
+                         types_article=types_article)
 
 
 @admin_article.route('/admin/article/edit', methods=['POST'])
 def valid_edit_article():
     mycursor = get_db().cursor()
-    nom = request.form.get('nom')
     id_article = request.form.get('id_article')
-    image = request.files.get('image', '')
-    type_article_id = request.form.get('type_article_id', '')
-    prix = request.form.get('prix', '')
+    nom = request.form.get('nom')
+    prix = request.form.get('prix')
+    stock = request.form.get('stock', 0)
     description = request.form.get('description')
-    sql = '''
-       requête admin_article_8
-       '''
-    mycursor.execute(sql, id_article)
-    image_nom = mycursor.fetchone()
-    image_nom = image_nom['image']
-    if image:
-        if image_nom != "" and image_nom is not None and os.path.exists(
-                os.path.join(os.getcwd() + "/static/images/", image_nom)):
-            os.remove(os.path.join(os.getcwd() + "/static/images/", image_nom))
-        # filename = secure_filename(image.filename)
-        if image:
-            filename = 'img_upload_' + str(int(2147483647 * random())) + '.png'
-            image.save(os.path.join('static/images/', filename))
-            image_nom = filename
+    type_article_id = request.form.get('type_article_id')
+    image = request.files.get('image', None)
 
-    sql = '''  requête admin_article_9 '''
-    mycursor.execute(sql, (nom, image_nom, prix, type_article_id, description, id_article))
+    if image:
+        sql = '''
+        UPDATE skin 
+        SET nom_skin = %s, prix_skin = %s, stock = %s, 
+            type_skin_id = %s, description = %s, image = %s
+        WHERE id_skin = %s
+        RETURNING image as old_image
+        '''
+        tuple_update = (nom, prix, stock, type_article_id, description, image.filename, id_article)
+    else:
+        sql = '''
+        UPDATE skin 
+        SET nom_skin = %s, prix_skin = %s, stock = %s, 
+            type_skin_id = %s, description = %s
+        WHERE id_skin = %s
+        '''
+        tuple_update = (nom, prix, stock, type_article_id, description, id_article)
+
+    mycursor.execute(sql, tuple_update)
+
+    if image:
+        image.save(os.path.join('static/images/', image.filename))
+        if mycursor.rowcount > 0:
+            old_image = mycursor.fetchone()['old_image']
+            if old_image and os.path.exists(os.path.join('static/images/', old_image)):
+                os.remove(os.path.join('static/images/', old_image))
 
     get_db().commit()
-    if image_nom is None:
-        image_nom = ''
-    message = u'article modifié , nom:' + nom + '- type_article :' + type_article_id + ' - prix:' + prix  + ' - image:' + image_nom + ' - description: ' + description
-    flash(message, 'alert-success')
+    flash(f'Article {nom} modifié avec succès', 'success')
     return redirect('/admin/article/show')
-
-
-
-
-
 
 
 @admin_article.route('/admin/article/avis/<int:id>', methods=['GET'])
@@ -242,6 +273,32 @@ def update_stock():
         flash('Le stock ne peut pas être négatif', 'alert-danger')
         return redirect('/admin/article/show')
         
+    sql = '''
+    UPDATE skin 
+    SET stock = %s 
+    WHERE id_skin = %s
+    '''
+    mycursor.execute(sql, (new_stock, id_article))
+    get_db().commit()
+    
+    flash('Stock mis à jour avec succès', 'alert-success')
+    return redirect('/admin/article/show')
+
+
+@admin_article.route('/admin/article/stock/update', methods=['POST'])
+def update_article_stock():
+    mycursor = get_db().cursor()
+    id_article = request.form.get('id_article')
+    new_stock = request.form.get('stock', type=int)
+    
+    if new_stock is None:
+        flash('Le stock doit être spécifié', 'alert-danger')
+        return redirect('/admin/article/show')
+        
+    if new_stock < 0:
+        flash('Le stock ne peut pas être négatif', 'alert-danger')
+        return redirect('/admin/article/show')
+    
     sql = '''
     UPDATE skin 
     SET stock = %s 
--- controllers/admin_commentaire.py
+++ controllers/admin_commentaire.py
@@ -8,57 +8,115 @@ from connexion_db import get_db
 admin_commentaire = Blueprint('admin_commentaire', __name__,
                         template_folder='templates')
 
-
 @admin_commentaire.route('/admin/article/commentaires', methods=['GET'])
 def admin_article_details():
     mycursor = get_db().cursor()
-    id_article =  request.args.get('id_article', None)
-    sql = '''    requête admin_type_article_1    '''
-    commentaires = {}
-    sql = '''   requête admin_type_article_1_bis   '''
-    article = []
-    sql = '''   requête admin_type_article_1_3   '''
-    nb_commentaires = []
-    return render_template('admin/article/show_article_commentaires.html'
-                           , commentaires=commentaires
-                           , article=article
-                           , nb_commentaires=nb_commentaires
-                           )
+    id_article = request.args.get('id_article', None)
+    
+    # Récupérer les informations de l'article et ses commentaires
+    sql = '''
+    SELECT 
+        s.id_skin as id_article,
+        s.nom_skin as nom,
+        s.image,
+        s.description,
+        COUNT(c.id_commentaire) as nb_commentaires
+    FROM skin s
+    LEFT JOIN commentaire c ON s.id_skin = c.skin_id
+    WHERE s.id_skin = %s
+    GROUP BY s.id_skin, s.nom_skin, s.image, s.description
+    '''
+    mycursor.execute(sql, (id_article,))
+    article = mycursor.fetchone()
+
+    # Récupérer les commentaires de l'article
+    sql = '''
+    SELECT 
+        c.id_commentaire,
+        c.commentaire,
+        c.date_publication,
+        c.note,
+        u.id_utilisateur,
+        u.nom as nom_utilisateur,
+        u.email
+    FROM commentaire c
+    JOIN utilisateur u ON c.utilisateur_id = u.id_utilisateur
+    WHERE c.skin_id = %s
+    ORDER BY c.date_publication DESC
+    '''
+    mycursor.execute(sql, (id_article,))
+    commentaires = mycursor.fetchall()
+
+    # Compter les commentaires non validés
+    sql = '''
+    SELECT COUNT(*) as nb_new
+    FROM commentaire 
+    WHERE skin_id = %s AND valide = 0
+    '''
+    mycursor.execute(sql, (id_article,))
+    nb_commentaires = mycursor.fetchone()
+
+    return render_template('admin/article/show_article_commentaires.html',
+                         commentaires=commentaires,
+                         article=article,
+                         nb_commentaires=nb_commentaires)
 
 @admin_commentaire.route('/admin/article/commentaires/delete', methods=['POST'])
 def admin_comment_delete():
     mycursor = get_db().cursor()
-    id_utilisateur = request.form.get('id_utilisateur', None)
+    id_commentaire = request.form.get('id_commentaire', None)
     id_article = request.form.get('id_article', None)
-    date_publication = request.form.get('date_publication', None)
-    sql = '''    requête admin_type_article_2   '''
-    tuple_delete=(id_utilisateur,id_article,date_publication)
-    get_db().commit()
-    return redirect('/admin/article/commentaires?id_article='+id_article)
+    
+    if not id_commentaire or not id_article:
+        flash('Erreur lors de la suppression du commentaire', 'alert-danger')
+        return redirect('/admin/article/commentaires?id_article=' + str(id_article))
 
+    sql = '''
+    DELETE FROM commentaire 
+    WHERE id_commentaire = %s
+    '''
+    mycursor.execute(sql, (id_commentaire,))
+    get_db().commit()
+    
+    flash('Commentaire supprimé avec succès', 'alert-success')
+    return redirect('/admin/article/commentaires?id_article=' + str(id_article))
 
 @admin_commentaire.route('/admin/article/commentaires/repondre', methods=['POST','GET'])
 def admin_comment_add():
     if request.method == 'GET':
-        id_utilisateur = request.args.get('id_utilisateur', None)
         id_article = request.args.get('id_article', None)
-        date_publication = request.args.get('date_publication', None)
-        return render_template('admin/article/add_commentaire.html',id_utilisateur=id_utilisateur,id_article=id_article,date_publication=date_publication )
+        id_commentaire_parent = request.args.get('id_commentaire', None)
+        return render_template('admin/article/add_commentaire.html',
+                             id_article=id_article,
+                             id_commentaire_parent=id_commentaire_parent)
 
     mycursor = get_db().cursor()
-    id_utilisateur = session['id_user']   #1 admin
     id_article = request.form.get('id_article', None)
-    date_publication = request.form.get('date_publication', None)
     commentaire = request.form.get('commentaire', None)
-    sql = '''    requête admin_type_article_3   '''
+    id_commentaire_parent = request.form.get('id_commentaire_parent', None)
+    
+    sql = '''
+    INSERT INTO commentaire(skin_id, utilisateur_id, commentaire, date_publication, valide, commentaire_parent_id)
+    VALUES (%s, %s, %s, NOW(), 1, %s)
+    '''
+    mycursor.execute(sql, (id_article, session['id_user'], commentaire, id_commentaire_parent))
     get_db().commit()
-    return redirect('/admin/article/commentaires?id_article='+id_article)
+    
+    flash('Réponse ajoutée avec succès', 'alert-success')
+    return redirect('/admin/article/commentaires?id_article=' + str(id_article))
 
-
-@admin_commentaire.route('/admin/article/commentaires/valider', methods=['POST','GET'])
+@admin_commentaire.route('/admin/article/commentaires/valider', methods=['GET'])
 def admin_comment_valider():
     id_article = request.args.get('id_article', None)
     mycursor = get_db().cursor()
-    sql = '''   requête admin_type_article_4   '''
+    
+    sql = '''
+    UPDATE commentaire 
+    SET valide = 1 
+    WHERE skin_id = %s AND valide = 0
+    '''
+    mycursor.execute(sql, (id_article,))
     get_db().commit()
-    return redirect('/admin/article/commentaires?id_article='+id_article)
+    
+    flash('Tous les commentaires ont été validés', 'alert-success')
+    return redirect('/admin/article/commentaires?id_article=' + str(id_article))
