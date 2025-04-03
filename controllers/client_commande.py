dfdda0a20996e6522bfe51ad87b0d84729694f94
amintt2 <amin2020e@gmail.com>
2025-04-03 14:31:43 +0200

    Refactor client_commande and client_coordonnee controllers for improved readability and functionality. Updated SQL queries for better clarity, added address handling in order processing, and enhanced user interface in templates for address management. Adjusted variable names for consistency and improved error handling in address-related routes.

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -12,8 +12,8 @@ client_commande = Blueprint('client_commande', __name__,
 # validation de la commande : partie 2 -- vue pour choisir les adresses (livraision et facturation)
 @client_commande.route('/client/commande/valide', methods=['POST'])
 def client_commande_valide():
-    mycursor = get_db().cursor()
-    id_client = session['id_user']
+    curseur = get_db().cursor()
+    identifiant_client = session['id_user']
     sql = ''' selection des articles d'un panier 
     '''
     articles_panier = []
@@ -34,48 +34,62 @@ def client_commande_valide():
 
 @client_commande.route('/client/commande/add', methods=['POST'])
 def client_commande_add():
-    mycursor = get_db().cursor()
-    id_client = session['id_user']
+    curseur = get_db().cursor()
+    identifiant_client = session['id_user']
+    
+    # Récupération des informations d'adresse
+    identifiant_adresse_livraison = request.form.get('adresse_livraison_id', None)
+    adresse_identique = request.form.get('adresse_identique', None)
+    
+    # Si l'adresse de facturation est identique à l'adresse de livraison
+    if adresse_identique == 'adresse_identique':
+        identifiant_adresse_facturation = identifiant_adresse_livraison
+    else:
+        identifiant_adresse_facturation = request.form.get('adresse_facturation_id', None)
     
     # Récupération des articles du panier
-    sql = ''' 
-        SELECT  lp.declinaison_id,
-                lp.quantite,
-                d.prix_declinaison * lp.quantite as prix
-        FROM ligne_panier lp
-        JOIN declinaison d ON lp.declinaison_id = d.id_declinaison
-        WHERE lp.utilisateur_id = %s
+    requete_sql = ''' 
+        SELECT  ligne_panier.declinaison_id AS identifiant_declinaison,
+                ligne_panier.quantite AS quantite,
+                declinaison.prix_declinaison * ligne_panier.quantite AS prix_total
+        FROM ligne_panier
+        JOIN declinaison ON ligne_panier.declinaison_id = declinaison.id_declinaison
+        WHERE ligne_panier.utilisateur_id = %s
     '''
-    mycursor.execute(sql, (id_client,))
-    items_ligne_panier = mycursor.fetchall()
+    curseur.execute(requete_sql, (identifiant_client,))
+    articles_panier = curseur.fetchall()
     
-    if not items_ligne_panier:
+    if not articles_panier:
         flash(u'Pas d\'articles dans le panier', 'alert-warning')
         return redirect('/client/article/show')
 
     # Création de la commande
-    sql = '''
+    requete_sql = '''
         INSERT INTO commande(
             date_achat,
             etat_id,
-            utilisateur_id
+            utilisateur_id,
+            adresse_livraison_id,
+            adresse_facturation_id
         ) VALUES (
             NOW(),
             1,
+            %s,
+            %s,
             %s
         )
     '''
-    mycursor.execute(sql, (id_client,))
+    curseur.execute(requete_sql, (identifiant_client, identifiant_adresse_livraison, identifiant_adresse_facturation))
     
-    # Récupération de l'id de la commande créée
-    sql = '''SELECT LAST_INSERT_ID() as last_insert_id'''
-    mycursor.execute(sql)
-    id_commande = mycursor.fetchone()['last_insert_id']
+    # Récupération de l'identifiant de la commande créée
+    requete_sql = '''SELECT LAST_INSERT_ID() AS identifiant_derniere_insertion'''
+    curseur.execute(requete_sql)
+    identifiant_commande = curseur.fetchone()['identifiant_derniere_insertion']
 
-    # Traitement des lignes du panier
-    for item in items_ligne_panier:
-        # Ajout ligne de commande
-        sql = '''
+    # Traitement des articles du panier
+    for article in articles_panier:
+        # Ajout d'une ligne de commande
+        requete_sql = '''
             INSERT INTO ligne_commande(
                 declinaison_id,
                 commande_id,
@@ -88,19 +102,19 @@ def client_commande_add():
                 %s
             )
         '''
-        mycursor.execute(sql, (
-            item['declinaison_id'],
-            id_commande,
-            item['prix'],
-            item['quantite']
+        curseur.execute(requete_sql, (
+            article['identifiant_declinaison'],
+            identifiant_commande,
+            article['prix_total'],
+            article['quantite']
         ))
 
-        # Suppression ligne panier
-        sql = '''
+        # Suppression de l'article du panier
+        requete_sql = '''
             DELETE FROM ligne_panier 
             WHERE declinaison_id = %s AND utilisateur_id = %s
         '''
-        mycursor.execute(sql, (item['declinaison_id'], id_client))
+        curseur.execute(requete_sql, (article['identifiant_declinaison'], identifiant_client))
 
     get_db().commit()
     flash(u'Commande validée avec succès', 'alert-success')
@@ -112,16 +126,18 @@ def client_commande_add():
 def client_commande_show():
     print('client_commande_show')
     
-    mycursor = get_db().cursor()
-    id_client = session['id_user']
-    sql = '''
+    curseur = get_db().cursor()
+    identifiant_client = session['id_user']
+    
+    # Récupération de toutes les commandes du client
+    requete_sql = '''
         SELECT 
-            commande.id_commande,
-            commande.date_achat,
-            etat.libelle_etat as libelle,
-            etat.id_etat as etat_id,
-            COUNT(ligne_commande.commande_id) as nbr_articles,
-            SUM(ligne_commande.prix * ligne_commande.quantite) as prix_total
+            commande.id_commande AS identifiant_commande,
+            commande.date_achat AS date_achat,
+            etat.libelle_etat AS libelle_etat,
+            etat.id_etat AS identifiant_etat,
+            COUNT(ligne_commande.commande_id) AS nombre_articles,
+            SUM(ligne_commande.prix * ligne_commande.quantite) AS prix_total_commande
         FROM commande
         INNER JOIN etat ON commande.etat_id = etat.id_etat
         LEFT JOIN ligne_commande ON commande.id_commande = ligne_commande.commande_id
@@ -129,38 +145,67 @@ def client_commande_show():
         GROUP BY commande.id_commande, commande.date_achat, etat.libelle_etat, etat.id_etat
         ORDER BY commande.date_achat DESC
         '''
-    mycursor.execute(sql, (id_client,))
-    commandes = mycursor.fetchall()
+    curseur.execute(requete_sql, (identifiant_client,))
+    commandes = curseur.fetchall()
 
     articles_commande = None
-    commande_adresses = None
-    id_commande = request.args.get('id_commande', None)
-    if id_commande != None:
-        print(id_commande)
-        sql = '''
+    informations_adresses_commande = None
+    identifiant_commande = request.args.get('id_commande', None)
+    
+    if identifiant_commande is not None:
+        print(identifiant_commande)
+        
+        # Récupération des articles de la commande
+        requete_sql = '''
             SELECT 
-                skin.nom_skin AS nom,
-                skin.image,
-                ligne_commande.quantite,
-                ligne_commande.prix,
-                (ligne_commande.prix * ligne_commande.quantite) AS prix_ligne,
-                usure.libelle_usure,
-                type_skin.libelle_type_skin,
-                special.libelle_special
+                skin.nom_skin AS nom_article,
+                skin.image AS image_article,
+                ligne_commande.quantite AS quantite_article,
+                ligne_commande.prix AS prix_unitaire,
+                (ligne_commande.prix * ligne_commande.quantite) AS prix_total_ligne,
+                usure.libelle_usure AS libelle_usure,
+                type_skin.libelle_type_skin AS libelle_type_skin,
+                special.libelle_special AS libelle_special
             FROM ligne_commande
-            INNER JOIN declinaison d ON ligne_commande.declinaison_id = d.id_declinaison
-            INNER JOIN skin ON d.skin_id = skin.id_skin
-            INNER JOIN usure ON d.usure_id = usure.id_usure
+            INNER JOIN declinaison ON ligne_commande.declinaison_id = declinaison.id_declinaison
+            INNER JOIN skin ON declinaison.skin_id = skin.id_skin
+            INNER JOIN usure ON declinaison.usure_id = usure.id_usure
             INNER JOIN type_skin ON skin.type_skin_id = type_skin.id_type_skin
-            INNER JOIN special ON d.special_id = special.id_special
+            INNER JOIN special ON declinaison.special_id = special.id_special
             WHERE ligne_commande.commande_id = %s
         '''
-        mycursor.execute(sql, (id_commande,))
-        articles_commande = mycursor.fetchall()
+        curseur.execute(requete_sql, (identifiant_commande,))
+        articles_commande = curseur.fetchall()
         
-    return render_template('client/commandes/show.html'
-                           , commandes=commandes
-                           , articles_commande=articles_commande
-                           , commande_adresses=commande_adresses
+        # Récupération des informations d'adresses pour la commande
+        requete_sql = '''
+            SELECT 
+                commande.adresse_livraison_id AS identifiant_adresse_livraison,
+                commande.adresse_facturation_id AS identifiant_adresse_facturation,
+                adresse_livraison.nom AS nom_livraison,
+                adresse_livraison.rue AS rue_livraison,
+                adresse_livraison.code_postal AS code_postal_livraison,
+                adresse_livraison.ville AS ville_livraison,
+                adresse_facturation.nom AS nom_facturation,
+                adresse_facturation.rue AS rue_facturation,
+                adresse_facturation.code_postal AS code_postal_facturation,
+                adresse_facturation.ville AS ville_facturation,
+                CASE 
+                    WHEN commande.adresse_livraison_id = commande.adresse_facturation_id 
+                    THEN 'adresse_identique' 
+                    ELSE 'adresse_differente' 
+                END AS adresse_identique
+            FROM commande
+            JOIN adresse AS adresse_livraison ON commande.adresse_livraison_id = adresse_livraison.id_adresse
+            JOIN adresse AS adresse_facturation ON commande.adresse_facturation_id = adresse_facturation.id_adresse
+            WHERE commande.id_commande = %s
+        '''
+        curseur.execute(requete_sql, (identifiant_commande,))
+        informations_adresses_commande = curseur.fetchone()
+        
+    return render_template('client/commandes/show.html',
+                           commandes=commandes,
+                           articles_commande=articles_commande,
+                           commande_adresses=informations_adresses_commande
                            )
 
--- controllers/client_coordonnee.py
+++ controllers/client_coordonnee.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+from datetime import datetime
 
 from connexion_db import get_db
 
@@ -13,47 +14,107 @@ client_coordonnee = Blueprint('client_coordonnee', __name__,
 def client_coordonnee_show():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    utilisateur=[]
-    return render_template('client/coordonnee/show_coordonnee.html'
-                           , utilisateur=utilisateur
-                         #  , adresses=adresses
-                         #  , nb_adresses=nb_adresses
+    
+    # Récupérer les informations de l'utilisateur
+    sql = "SELECT * FROM utilisateur WHERE id_utilisateur = %s"
+    mycursor.execute(sql, (id_client,))
+    utilisateur = mycursor.fetchone()
+    
+    # Récupérer les adresses de l'utilisateur
+    sql = """
+    SELECT adresse.*, 
+           (SELECT COUNT(*) FROM commande WHERE commande.adresse_livraison_id = adresse.id_adresse) AS nbr_commandes
+    FROM adresse
+    WHERE adresse.utilisateur_id = %s
+    """
+    mycursor.execute(sql, (id_client,))
+    adresses = mycursor.fetchall()
+    
+    # Ajouter des valeurs par défaut pour valide et favori
+    for adresse in adresses:
+        adresse['valide'] = 1  # Toutes les adresses sont considérées comme valides
+        adresse['favori'] = 0  # Par défaut, aucune adresse n'est favorite
+    
+    # Compter le nombre total d'adresses (toutes sont valides)
+    nb_adresses = len(adresses)
+    nb_adresses_tot = nb_adresses
+
+    return render_template('client/coordonnee/show_coordonnee.html',
+                           utilisateur=utilisateur,
+                           adresses=adresses,
+                           nb_adresses=nb_adresses,
+                           nb_adresses_tot=nb_adresses_tot
                            )
 
 @client_coordonnee.route('/client/coordonnee/edit', methods=['GET'])
 def client_coordonnee_edit():
     mycursor = get_db().cursor()
     id_client = session['id_user']
+    
+    # Récupérer les informations de l'utilisateur
+    sql = "SELECT * FROM utilisateur WHERE id_utilisateur = %s"
+    mycursor.execute(sql, (id_client,))
+    utilisateur = mycursor.fetchone()
 
-    return render_template('client/coordonnee/edit_coordonnee.html'
-                           #,utilisateur=utilisateur
-                           )
+    return render_template('client/coordonnee/edit_coordonnee.html',
+                           utilisateur=utilisateur)
 
 @client_coordonnee.route('/client/coordonnee/edit', methods=['POST'])
 def client_coordonnee_edit_valide():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    nom=request.form.get('nom')
+    nom = request.form.get('nom')
     login = request.form.get('login')
     email = request.form.get('email')
-
-    utilisateur = None
-    if utilisateur:
-        flash(u'votre cet Email ou ce Login existe déjà pour un autre utilisateur', 'alert-warning')
-        return render_template('client/coordonnee/edit_coordonnee.html'
-                               #, user=user
-                               )
-
-
+    
+    # Vérifier si le login ou l'email existe déjà
+    sql = """
+    SELECT * FROM utilisateur 
+    WHERE (login = %s OR email = %s) 
+    AND id_utilisateur != %s
+    """
+    mycursor.execute(sql, (login, email, id_client))
+    utilisateur_existant = mycursor.fetchone()
+    
+    if utilisateur_existant:
+        flash(u'Cet Email ou ce Login existe déjà pour un autre utilisateur', 'alert-warning')
+        
+        # Récupérer les informations de l'utilisateur actuel
+        sql = "SELECT * FROM utilisateur WHERE id_utilisateur = %s"
+        mycursor.execute(sql, (id_client,))
+        utilisateur = mycursor.fetchone()
+        
+        return render_template('client/coordonnee/edit_coordonnee.html',
+                               utilisateur=utilisateur)
+    
+    # Mettre à jour les informations de l'utilisateur
+    sql = """
+    UPDATE utilisateur 
+    SET nom = %s, login = %s, email = %s
+    WHERE id_utilisateur = %s
+    """
+    mycursor.execute(sql, (nom, login, email, id_client))
     get_db().commit()
+    
     return redirect('/client/coordonnee/show')
 
 
-@client_coordonnee.route('/client/coordonnee/delete_adresse',methods=['POST'])
+@client_coordonnee.route('/client/coordonnee/delete_adresse', methods=['POST'])
 def client_coordonnee_delete_adresse():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    id_adresse= request.form.get('id_adresse')
+    id_adresse = request.form.get('id_adresse')
+    
+    # Vérifier que l'adresse appartient bien à l'utilisateur
+    sql = "SELECT * FROM adresse WHERE id_adresse = %s AND utilisateur_id = %s"
+    mycursor.execute(sql, (id_adresse, id_client))
+    adresse = mycursor.fetchone()
+    
+    if adresse:
+        # Supprimer l'adresse
+        sql = "DELETE FROM adresse WHERE id_adresse = %s"
+        mycursor.execute(sql, (id_adresse,))
+        get_db().commit()
 
     return redirect('/client/coordonnee/show')
 
@@ -61,19 +122,36 @@ def client_coordonnee_delete_adresse():
 def client_coordonnee_add_adresse():
     mycursor = get_db().cursor()
     id_client = session['id_user']
+    
+    # Récupérer les informations de l'utilisateur
+    sql = "SELECT * FROM utilisateur WHERE id_utilisateur = %s"
+    mycursor.execute(sql, (id_client,))
+    utilisateur = mycursor.fetchone()
 
-    return render_template('client/coordonnee/add_adresse.html'
-                           #,utilisateur=utilisateur
-                           )
+    return render_template('client/coordonnee/add_adresse.html',
+                           utilisateur=utilisateur,
+                           nom="",
+                           rue="",
+                           code_postal="",
+                           ville="")
 
-@client_coordonnee.route('/client/coordonnee/add_adresse',methods=['POST'])
+@client_coordonnee.route('/client/coordonnee/add_adresse', methods=['POST'])
 def client_coordonnee_add_adresse_valide():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    nom= request.form.get('nom')
+    nom = request.form.get('nom')
     rue = request.form.get('rue')
     code_postal = request.form.get('code_postal')
     ville = request.form.get('ville')
+    
+    # Insérer la nouvelle adresse
+    sql = """
+    INSERT INTO adresse (nom, rue, code_postal, ville, utilisateur_id)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+    mycursor.execute(sql, (nom, rue, code_postal, ville, id_client))
+    get_db().commit()
+    
     return redirect('/client/coordonnee/show')
 
 @client_coordonnee.route('/client/coordonnee/edit_adresse')
@@ -81,20 +159,48 @@ def client_coordonnee_edit_adresse():
     mycursor = get_db().cursor()
     id_client = session['id_user']
     id_adresse = request.args.get('id_adresse')
+    
+    # Récupérer les informations de l'utilisateur
+    sql = "SELECT * FROM utilisateur WHERE id_utilisateur = %s"
+    mycursor.execute(sql, (id_client,))
+    utilisateur = mycursor.fetchone()
+    
+    # Récupérer les informations de l'adresse
+    sql = "SELECT * FROM adresse WHERE id_adresse = %s AND utilisateur_id = %s"
+    mycursor.execute(sql, (id_adresse, id_client))
+    adresse = mycursor.fetchone()
+    
+    if not adresse:
+        # Si l'adresse n'existe pas ou n'appartient pas à l'utilisateur
+        return redirect('/client/coordonnee/show')
 
-    return render_template('/client/coordonnee/edit_adresse.html'
-                           # ,utilisateur=utilisateur
-                           # ,adresse=adresse
-                           )
+    return render_template('client/coordonnee/edit_adresse.html',
+                           utilisateur=utilisateur,
+                           adresse=adresse)
 
-@client_coordonnee.route('/client/coordonnee/edit_adresse',methods=['POST'])
+@client_coordonnee.route('/client/coordonnee/edit_adresse', methods=['POST'])
 def client_coordonnee_edit_adresse_valide():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    nom= request.form.get('nom')
+    nom = request.form.get('nom')
     rue = request.form.get('rue')
     code_postal = request.form.get('code_postal')
     ville = request.form.get('ville')
     id_adresse = request.form.get('id_adresse')
+    
+    # Vérifier que l'adresse appartient bien à l'utilisateur
+    sql = "SELECT * FROM adresse WHERE id_adresse = %s AND utilisateur_id = %s"
+    mycursor.execute(sql, (id_adresse, id_client))
+    adresse = mycursor.fetchone()
+    
+    if adresse:
+        # Mettre à jour l'adresse
+        sql = """
+        UPDATE adresse
+        SET nom = %s, rue = %s, code_postal = %s, ville = %s
+        WHERE id_adresse = %s
+        """
+        mycursor.execute(sql, (nom, rue, code_postal, ville, id_adresse))
+        get_db().commit()
 
     return redirect('/client/coordonnee/show')
